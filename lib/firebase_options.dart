1956e0cfe3c4f42c4bf367ec44668d3b25d75192
Ubuntu <ubuntu@localhost.localdomain>
2026-09-13 14:21:51 +0530

    Fix Firebase API key and APK workflow

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
+    apiKey: 'AIzaSyBXEoyq38fC3rb_eBzrfWboz0fsmkU9ImM',
     appId: '1:961292358991:android:2a07ff0615a940b0e5e13a',
     messagingSenderId: '961292358991',
     projectId: 'jigri-yaar-3c783',
